440b96109da2c8b0ec6d57ef1b18706f2e2bfb02
Rocio Grajales <rocioegrajales@gmail.com>
2024-07-16 19:08:08 -0500

    Ultimos cambios, app finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,88 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-import './App.css';
-import { useAccount } from 'wagmi';
+import { useAccount, useReadContract, useWriteContract } from 'wagmi';
+import { abi } from './assets/abis/FSCTokenAbi';
+import { FSC_CONTRACT_ADDRESS } from "./assets/constants/index";
+import { useState } from 'react';
+import { waitForTransactionReceipt } from 'viem/actions';
+import { config } from "./main";
+import { toast } from "react-toastify";
 
 function App() { 
-  const { address, } = useAccount();
+  const { address, isConnected } = useAccount();
+  const [ isMinting, setIsMinting] = useState(false);
+
+  const { data, isLoading, refetch } = useReadContract({
+    abi,
+    address: FSC_CONTRACT_ADDRESS,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  const {writeContractAsync} = useWriteContract()
+
+  const handleMint = async () => {
+    setIsMinting(true); 
+
+  try{
+    const txHash = await writeContractAsync({
+      abi,
+      address: FSC_CONTRACT_ADDRESS,
+      functionName: "mint",
+      args: [address, 200],
+    })
+
+    await waitForTransactionReceipt(config, {
+      confirmations: 1,
+      hash: txHash,
+    })
+
+    setIsMinting(false);
+    toast.success("Minted 200 FSC tokens");
+    refetch();
+
+  }catch (error) {
+    console.error(error);
+    toast.error("failed to mint FSC tokens");
+    setIsMinting(false);
+  }
+};
 
   return (
     <main className="w-full flex justify-center items-center min-h-svh flex-col">
-      <h1 className="text-4xl font-bold">🚀 CFE Token Faucet 🚀</h1>    
-        <ConnectButton />    
-      {/* </div> */}
-    </main>
-  )
-};
+      <h1 className="text-5xl font-bold text-green-700 ">FSC TOKEN</h1>
+      <h1 className="text-5xl font-bold text-green-700">Faucet</h1>    
+      <div className="my-5 p-4 flex flex-col items-center gap-5 rounded border border-gray-400">
+        <ConnectButton />        
+        {isConnected ? (
+          <div className="space-y-5">
+            <p>
+                <span>Balance:{" "}</span>
+              {isLoading ? (
+                <span className="opacity-50">Loading...</span>
+              ) : (
+                data?.toString()
+              )}
+            </p>
 
-export default App
+            <button className="px-3 py-1 font-semibold bg-green-600 rounded-xl disabled:opacity-50"
+              disabled={isMinting}
+              onClick={handleMint}>
+              {isMinting ? "Minting..." : "Mint tokens"}
+             </button>
+          </div>
+        ) : (
+          <div>Please connect your wallet to use the faucet</div>
+        )}
+      </div>
+
+      <footer>
+          <h2>&#169;2024 - Todos los derechos reservdos - REGM</h2>
+        </footer>
+        
+    </main>
+
+    
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import '@rainbow-me/rainbowkit/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { arbitrum, arbitrumSepolia } from 'viem/chains';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
 
-const config = getDefaultConfig({
+export const config = getDefaultConfig({
   appName: 'My FrontSmart App',
   projectId: import.meta.env.VITE_PROJECT_ID,
-  chains: [arbitrumSepolia, arbitrum],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  chains: [arbitrumSepolia, arbitrum] 
 });
 
 const queryClient = new QueryClient();
@@ -27,4 +28,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </QueryClientProvider>
     </WagmiProvider>   
   </React.StrictMode>
-)
+);
